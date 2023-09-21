6a62524491f79bf8f44a85bfde6b1544383a22cf
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2023-09-21 18:54:30 +0200

    Added handle errors functionality

--- src/App.js
+++ src/App.js
@@ -10,25 +10,29 @@ function App() {
   );
 
   const [notesLoading, setNotesLoadding] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     let setNotesTimeout;
 
     const handleSetNotesPromise = async () => {
+      setError(null);
       setNotesLoadding(true);
+      try {
+        const promise = new Promise((resolve, reject) => {
+          setNotesTimeout = setTimeout(() => {
+            localStorage.setItem("notes", JSON.stringify(notes));
+            resolve(true);
+          }, 1500);
+        });
 
-      const promise = new Promise((resolve) => {
-        setNotesTimeout = setTimeout(() => {
-          localStorage.setItem("notes", JSON.stringify(notes));
-          resolve(true);
-        }, 1500);
-      });
-
-      await promise;
-
-      setNotesLoadding(false);
+        await promise;
+        setNotesLoadding(false);
+      } catch (err) {
+        setError(err.message);
+        setNotesLoadding(false);
+      }
     };
-
     handleSetNotesPromise();
 
     return () => clearTimeout(setNotesTimeout);
@@ -41,14 +45,17 @@ function App() {
   const deleteNote = (transferedNote) => {
     setNotes(notes.filter((note) => note.id !== transferedNote.id));
   };
+
   return (
     <div className="App">
       <h1 className={styles.h1}>This is your Note List</h1>
       <NoteForm create={createNewNote} />
       {notesLoading ? (
-        <h2 className={styles.loading}>Loadding...</h2>
-      ) : (
+        <h2 className={styles.loading}>Loading...</h2>
+      ) : !error ? (
         <NoteList notes={notes} deleteNote={deleteNote} />
+      ) : (
+        <p className={styles.error}>{error}</p>
       )}
     </div>
   );
